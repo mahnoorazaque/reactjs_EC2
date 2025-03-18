83cbcd4cbc504a75c4afb29ddbbab7ff7a587fdd
Mahnoor <mahnooorfatimah@gmail.com>
2025-03-17 23:21:00 -0700

    Update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Enables IPv4 and IPv6
+    port: 3000, // Set port for the frontend
+  }
+});
